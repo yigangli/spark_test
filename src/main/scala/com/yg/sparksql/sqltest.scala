72c1254ecde1541ff13514687f284fc57426ce17
liyigang <18935117025@163.com>
2019-11-04 15:20:54 +0800

    update sqltest file path

--- src/main/scala/com/yg/sparksql/sqltest.scala
+++ src/main/scala/com/yg/sparksql/sqltest.scala
@@ -8,8 +8,8 @@ object sqltest {
   def main(args: Array[String]): Unit = {
     val conf: SparkConf = new SparkConf().setMaster("local").setAppName(this.getClass.getSimpleName)
     val sc: SparkContext = new SparkContext(conf)
-    sc.setLogLevel("OFF")
-    val rdd1: RDD[String] = sc.textFile("E:\\workspace\\testspark\\src\\file\\word.txt")
+    sc.setLogLevel("ERROR")
+    val rdd1: RDD[String] = sc.textFile("src/file/word.txt")
     rdd1.foreach(println)
   }
 }
